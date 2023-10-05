fc5f0f12dd51fb654c0bb0b4a29ed689385a173f
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2023-10-05 12:21:15 +0600

    create news page and add card

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,7 @@ import Root from "../Layout/Root";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import News from "../Pages/News/News";
 
 const router = createBrowserRouter([
     {
@@ -11,7 +12,12 @@ const router = createBrowserRouter([
       children: [
         {
             path: '/',
-            element: <Home></Home>
+            element: <Home></Home>,
+            loader: () => fetch('news.json')
+        },
+        {
+          path: '/news/:id',
+          element: <News></News>
         },
         {
           path: '/login',
